39f3a19df0ba3c92ec57c4119fa997f75009b59f
Saiful Islam Sajon <saifulislamsajon@gmail.com>
2020-01-07 13:16:28 +0600

    Login, Game, Splash Screen Has done

---
+++ app/src/main/java/com/example/collegecounselor/SplashScreenActivity.kt
@@ -0,0 +1,28 @@
+package com.example.collegecounselor
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.lang.Exception
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splashscreen)
+
+        val background = object: Thread(){
+            override fun run() {
+                super.run()
+                try {
+                    Thread.sleep(5000)
+                    val intent = Intent(baseContext, MainActivity::class.java)
+                    startActivity(intent)
+                }catch (e:Exception){
+                    e.printStackTrace()
+                }
+            }
+        }
+        background.start()
+    }
+}
